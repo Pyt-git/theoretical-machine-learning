21d3155b31d910fb1711f5406e10e67fc2875472
Pyt-git <weicky0804@gmail.com>
2026-07-01 14:54:43 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,10 @@
+from optimizers.pipeline import OptimizerPipeline
+from optimizers.adamw import AdamW
+from optimizers.weight_decay import WeightDecay
+from optimizers.lr_schedules import WarmupCosine
 
+optimizer = OptimizerPipeline(
+  adamw = AdamW(beta1 = 0.9, beta2 = 0.999, eps = 1e-8),
+  weight_decay = WeightDecay(wd = 0.01), 
+  lr_schedules = WarmupCosine(warmup_steps = 100, total_steps = 1000, base_lr = 0.001)
+)
