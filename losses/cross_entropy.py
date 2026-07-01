84297d0b1e9ab0104898fcc5f6b9c714e7f446d6
Pyt-git <weicky0804@gmail.com>
2026-07-01 15:45:32 -0400

    Update cross_entropy.py

--- losses/cross_entropy.py
+++ losses/cross_entropy.py
@@ -1,1 +1,13 @@
+import numpy as np
+from models.sigmoid import sigmoid
+from validate_y import validate_y
 
+def bce_loss(y):
+    eps = 1e-12
+    sigma = np.clip(sigma, eps, 1 - eps)
+
+    L = -[y * np.log(sigma) + (1 - y) * np.log(1 - sigma)]
+
+    def gradient_bce(sigma, y): 
+        return sigma - y 
+    return L
