00c0764c51c1d92ac922aca04dba461ab201c132
Pyt-git <weicky0804@gmail.com>
2026-07-02 17:45:26 -0400

    Update ddpm_sample.py

--- generative/ddpm/ddpm_sample.py
+++ generative/ddpm/ddpm_sample.py
@@ -12,3 +12,17 @@ def reverse_step_eps(xt,t, layers):
     eps_hat = eps_model(xt, tt, layers)
 
     mean = (1.0 / np.sqrt(alpha_t)) * (xt - (beta_t / np.sqrt(1.0 - ab_t)) * eps_hat)
+
+    if t > 0: 
+        noise = np.random.normal(size = xt.shape)
+        var = beta_t * (1.0 - alpha_bar[t - 1]) / (1.0 - ab_t)
+        return mean + np.sqrt(var) * noise
+    else: 
+        return mean
+
+def sample_reverse_eps(n_samples, layers): 
+    xt = np.random.normal(size=(n_samples, 2))
+    for t in reversed(range(T)): 
+        xt = reverse_step_eps(xt, t, layers)
+    return xt
+        
